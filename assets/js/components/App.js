9ec48f9f65c59359e88c96a8bfc30486b64dd68c
branweb1 <branweb1@gmail.com>
2015-09-09 13:24:14 -0700

    added ajax searchbar

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -1,12 +1,34 @@
 var React = require('react');
+var SearchForm = require('./SearchForm');
 
 module.exports = React.createClass({
+
+  getAnswer: function(){
+    // open request
+    var request = new XMLHttpRequest();
+
+    // set request to url, true mean do it async
+    request.open("GET", "/lifedecision", true);
+
+    // onload flag fired when, say, DOM content loaded or request is complete
+    request.onload = function(){
+        if (request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(request.responseText);
+            console.log("successful responose", data);
+        } else {
+            console.log("hit server, but there was an error");
+        }
+    };
+
+    request.send();
+  },
+
   render: function(){
     return (
       <div>
         <h1>Hello from react</h1>
+        <SearchForm search={this.getAnswer} />
       </div>
     )
   }
 });
-
